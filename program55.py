ae64dc22742917db0604145b0c75a9f69bf5dfc5
vaib204 <vaibhavkulkarni614@gmail.com>
2025-08-17 00:56:22 +0530

    Add files via upload

---
+++ program55.py
@@ -0,0 +1,28 @@
+def Display(row,col):
+  if(row != col):
+    print("Invalid input")
+    return
+  for i in range(1,row+1,1):
+    for j in range(1,col+1,1):
+      if(i == j):
+       print("$",end="\t")
+      elif(i > j):
+       print("*",end="\t")
+
+
+    print()  
+
+
+def main():
+  print("Enter row")
+  value1= int(input())
+
+  print("Enter col")
+  value2= int(input())
+
+  Display(value1,value2)
+
+if  __name__ == "__main__":
+  main()
+
+
---
+++ program57.py
@@ -0,0 +1,27 @@
+def Display(row,col):
+  if(row != col):
+    print("Invalid input")
+    return
+  for i in range(1,row+1,1):
+    for j in range(1,col+1,1):
+      if(i == 1 or j == 1 or i == row or j ==col):
+        print("*",end="\t")
+      else:
+        print(" ",end= "\t")  
+
+
+    print()  
+
+def main():
+  print("Enter row")
+  value1= int(input())
+
+  print("Enter col")
+  value2= int(input())
+
+  Display(value1,value2)
+
+if  __name__ == "__main__":
+  main()
+
+
